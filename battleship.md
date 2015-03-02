d3dce079318c0bb33fb8a20c9b5b2a7fcefd45dd
Saethor <saethor94@gmail.com>
2015-03-02 10:28:20 +0000

    First draft of the game

---
+++ battleship.md
@@ -0,0 +1,4 @@
+# Battleship
+
+- Two arrays with the board for each player and where there ships are. 
+- Two arrays wich determine where player has shot so he can't shot the same spot twice
---
+++ game.js
@@ -0,0 +1,83 @@
+(function() {
+
+var yourTable     = document.getElementById('you');
+var computerTable = document.getElementById('computer');
+
+
+// --===================================================-- 
+// -- Boards setup
+// --===================================================--
+var yourBoardSetup = [['','','','','','#','#','','',''],
+                     ['','','','','','','','','',''],
+                     ['','','','','','','','','',''],
+                     ['','#','#','#','#','','','','',''],
+                     ['#','','','','','','','','',''],
+                     ['#','','','','','','','','',''],
+                     ['#','','','','','#','#','#','',''],
+                     ['#','','','','','','','','',''],
+                     ['','','#','#','#','#','#','','',''],
+                     ['','','','','','','','','','']];
+
+var computerSetup = [['','','','','','#','#','#','',''],
+                     ['','','','','','','','','',''],
+                     ['','#','','','','','','','',''],
+                     ['','#','','','#','','','','#',''],
+                     ['','#','','','#','','','','#',''],
+                     ['','#','','','#','','','','',''],
+                     ['','','','','#','','','','',''],
+                     ['','','','','','','','','',''],
+                     ['','','','','','','','','',''],
+                     ['','','','#','#','#','#','#','','']];
+
+
+
+// --===================================================-- 
+// -- Function for drawing playerboards
+// --===================================================-- 
+function creatBoard(boardArray, tableID) 
+{
+    for (var i = 0; i < boardArray.length; i++) 
+    {
+        // Creates each row on the board and appends it to the table
+        var row = document.createElement("tr");
+        tableID.appendChild(row);
+
+        // Creates <td> and ands it to each row
+        for (var j = 0; j < boardArray[i].length; j++) 
+        {
+            var tdata = document.createElement("td");
+
+            // Checks if td represents part of a ship and ads class if it is your ship
+            // If it not your ship it adds a event listener
+            if (boardArray[i][j] == '#')
+            {
+                if (tableID == yourTable) 
+                {
+                    tdata.setAttribute("class", "ship");
+                } 
+                else 
+                {
+                    tdata.addEventListener("click", function() {
+                        this.setAttribute("class", "hit");
+                    });
+                }
+                
+            }
+            else 
+            {
+                tdata.addEventListener("click", function() {
+                    this.setAttribute("class", "no-hit");
+                });
+            }
+
+            // Appends <td> to <tr>
+            row.appendChild(tdata);
+        }
+    }
+}
+
+creatBoard(yourBoardSetup, yourTable);
+creatBoard(computerSetup, computerTable);
+
+
+}());
